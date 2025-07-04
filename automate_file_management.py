a3b8743cb0d43e8bf44fba831d704a0f9b7f2d70
jassim-hub <jassimkasule@gmail.com>
2025-07-04 07:06:11 +0300

    |||

--- automate_file_management.py
+++ automate_file_management.py
@@ -1,53 +1,43 @@
-# Python script to automate file management tasks
-import os  # This script automates file management tasks such as organizing files into directories based on their extensions.
+# Python script to automate file management tasks such as organizing files into directories based on their extensions, renaming files, and deleting old files.
+import os
 import shutil
 
-# Define the path to your download directory
-downloads_folder = "C:\\Users\\jassi\\Downloads"
 
-# Define target folder for different file types
+# Define the path to your download directory
+downloads_folder = '/home/jeff/Downloads'
+
+
+# Define target folders for different file types
 folders = {
-    "images": [".jpg", ".jpeg", ".png", ".gif"],
-    "documents": [
-        ".pdf",
-        ".docx",
-        ".txt",
-        ".xlsx",
-        ".pptx",
-        ".odt",
-        ".md",
-        ".rtf",
-        ".csv",
-        ".xml",
-        ".json",
-        ".html",
-        ".torrent",
-        ".xls",
-    ],
-    "videos": [".mp4", ".avi", ".mov"],
-    "audio": [".mp3", ".wav", ".aac"],
-    "archives": [".zip", ".tar", ".gz"],
-    "scripts": [".py", ".js", ".sh"],
-    "installers": [".exe", ".msi"],
-    "others": [],
+    'images': ['.jpg', '.jpeg', '.png', '.gif'],
+    'documents': ['.pdf', '.docx', '.txt', '.xlsx'],
+    'videos': ['.mp4', '.avi', '.mov'],
+    'audio': ['.mp3', '.wav', '.flac'],
+    'archives': ['.zip', '.rar', '.tar'],
+    'scripts': ['.py', '.sh', '.js'],
+    'installers': ['.exe', '.msi'],
+    'others': []
 }
 
-# Create target folders if they do not exist
+# Create target folders if they don't exist
 for folder in folders:
     folder_path = os.path.join(downloads_folder, folder)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
+
 # Loop through files in the downloads folder
 for filename in os.listdir(downloads_folder):
-    filepath = os.path.join(downloads_folder, filename)
-    # skip directories
-    if os.path.isdir(filepath):  # Check if it is a file
+    file_path = os.path.join(downloads_folder, filename)
+
+    # Skip directories
+    if os.path.isdir(file_path):
         continue
+
     # Check file extension and move to the appropriate folder
     for folder, extensions in folders.items():
         if any(filename.lower().endswith(ext) for ext in extensions):
             target_folder = os.path.join(downloads_folder, folder)
-            shutil.move(filepath, target_folder)
-            print(f"Moved {filename} to {target_folder}")
+            shutil.move(file_path, target_folder)
+            print(f'Moved {filename} to {target_folder}')
             break
